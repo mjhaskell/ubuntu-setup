994fb5dd25bd199c118c163dcca3962cc490c14d
Mat Haskell <mhaskell9@gmail.com>
2019-02-20 16:35:26 -0700

    Install control library

--- install_pips.sh
+++ install_pips.sh
@@ -7,6 +7,6 @@ pip2 install pyopencl pytesseract tesserocr jupyter --user
 
 pip3 install ipython numpy scipy pygame matplotlib pyside2 pybind11 --user
 pip3 install pyqt5 pyqtgraph pyopengl pyopengl_accelerate --user
-pip3 install pyopencl pytesseract tesserocr jupyter --user
+pip3 install pyopencl pytesseract tesserocr jupyter control --user
 
 echo_green "python libs installed"
