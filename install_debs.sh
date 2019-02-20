f25f3cedfd32eb1a37b730bd46769e391cd04e07
Mat Haskell <mhaskell9@gmail.com>
2019-02-20 16:33:29 -0700

    Changed to use dpkg not gdebi

--- install_debs.sh
+++ install_debs.sh
@@ -2,16 +2,18 @@
 
 echo_blue "installing .deb applications"
 
-sudo apt install gdebi
+#sudo apt install gdebi
 
 wget -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo gdebi google-chrome.deb
+sudo dpkg -i google-chrome.deb
 echo_green "google chrome installed"
 
 wget -O mendeley.deb https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
-sudo gdebi mendeley.deb
+sudo dpkg -i mendeley.deb
 echo_green "mendeley installed"
 
 wget -O slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-3.1.0-amd64.deb"
-sudo gdebi slack.deb
+sudo dpkg -i slack.deb
 echo_green "slack installed"
+
+sudo apt install -f
