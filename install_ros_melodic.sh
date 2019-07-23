a9ace3269d0b8d0754da101a1977d26506558dbf
mhask94 <32270030+mhask94@users.noreply.github.com>
2019-07-23 09:25:49 -0400

    updated ROS key

--- install_ros_melodic.sh
+++ install_ros_melodic.sh
@@ -6,9 +6,10 @@ echo_blue "Setup your sources.list"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-echo_blue "Setup up your keys"
+echo_blue "Set up up your keys"
 # this key could change if ROS ever updates it for whatever reason
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+# sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 echo_blue "Installation"
 
