469139c91667882abfadf4408d98bc31ff42842e
Mat Haskell <mhaskell9@gmail.com>
2019-09-03 13:22:33 -0600

    Added vtk

--- install_pips.sh
+++ install_pips.sh
@@ -5,7 +5,7 @@ echo_blue "pip installing python libs"
 pip2 install ipython numpy scipy pygame matplotlib pyside2 pybind11 --user
 pip2 install pyopencl pytesseract tesserocr jupyter --user
 
-pip3 install ipython numpy scipy pygame matplotlib pyside2 pybind11 --user
+pip3 install ipython numpy scipy pygame matplotlib pyside2 pybind11 vtk --user
 pip3 install pyqt5 pyqtgraph pyopengl pyopengl_accelerate --user
 pip3 install pyopencl pytesseract tesserocr jupyter control --user
 
