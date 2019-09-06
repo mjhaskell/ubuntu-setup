44c10aa4806058f8c5af3262388832ef91432c5e
Mat Haskell <mhaskell9@gmail.com>
2019-09-06 12:47:49 -0600

    Finished CUDA install script

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-echo_blue "installing cuda"
+if lspci -v | grep VGA | grep -q NVIDIA; then
+    echo_blue "Getting NVIDIA graphics drivers"
+    sudo add-apt-repository ppa:graphics-drivers/ppa
+    sudo apt update
 
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
+################################################################################
+#  This section of the script came from https://developer.nvidia.com/cuda-downloads
+#  It will need to be updated as newer versions come out
+    echo_blue "installing cuda"
+    mkdir ~/software/cuda
+    cd ~/software/cuda
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+    sudo apt install ./cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+    sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
+    sudo apt update
+    sudo apt -y install cuda
+################################################################################
+        
+    cd ~/scripts
 
-mkdir ~/software/cuda
-cd ~/software/cuda
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
-sudo apt install ./cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
-sudo apt update
-sudo apt -y install cuda
+    str=$(grep -o 'X Driver...........' /var/log/Xorg.0.log)
+    version=$(echo $str | cut -c11-19)
 
-cd ~/scripts
-
-echo_green "eigen installed"
+    # check that NVIDIA driver version matches the deb file above
+    if [ "$version" = "418.87.00" ]; then
+        echo_purple "NVIDIA driver version: $version"
+        echo_purple "CUDA version: 10.1.243"
+        echo_green "cuda installed"
+    else
+        echo_red "[ERROR] NVIDIA driver version is not 418.87.00"
+        echo_red "\tCheck CUDA and NVIDIA driver installations"
+    fi 
+else
+    echo_blue "No NVIDIA GPU found - not installing cuda"
+fi
