d65271de1fcb5c1f22f6f1882f2d32b5ec2683e3
Mat Haskell <mhaskell9@gmail.com>
2020-05-03 18:16:51 -0600

    Added python linters

--- install_pips.sh
+++ install_pips.sh
@@ -8,5 +8,6 @@ pip2 install pyopencl pytesseract tesserocr jupyter --user
 pip3 install ipython numpy scipy pygame matplotlib pyside2 pybind11 vtk --user
 pip3 install pyqt5 pyqtgraph pyopengl pyopengl_accelerate gnupg --user
 pip3 install pyopencl pytesseract tesserocr jupyter control --user
+pip3 install pylint autopep8 --user
 
 echo_green "python libs installed"
