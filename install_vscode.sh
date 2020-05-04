b52ae83c424206080f2276d391129642ca3129d7
Mat Haskell <mhaskell9@gmail.com>
2020-05-03 18:15:17 -0600

    Added VSCode install script

---
+++ install_vscode.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo_blue "Installing VSCode"
+
+wget -O install_vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo apt install ./install_vscode.deb
+rm install_vscode.deb
+
+# C/C++ extension
+code --install-extension ms-vscode.cpptools
+code --install-extension twxs.cmake
+
+# Python extension
+code --install-extension ms-python.python
+
+# Vim extension
+code --install-extension vscodevim.vim
+
+# IntelliCode extension
+code --install-extension visualstudioexptteam.vscodeintellicode
+
+echo_green "VSCode installed with extensions"
+
