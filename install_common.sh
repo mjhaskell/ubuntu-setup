aaa0364a2def37a0cf10e91cafe4d4ad5e57ae75
Mat Haskell <mhaskell9@gmail.com>
2020-05-28 01:09:10 -0600

    Added simplescreenrecorder

--- install_common.sh
+++ install_common.sh
@@ -13,7 +13,7 @@ sudo apt install -y python-dev python-pip python3-dev python3-pip python3-tk
 sudo apt install -y pybind11-dev libtool  
 sudo apt install -y libffi6 libffi-dev 
 sudo apt install -y nmap cifs-utils
-sudo apt install -y simplescreenrecorder vlc ffmpeg
+sudo apt install -y gnome-screenshot simplescreenrecorder vlc ffmpeg
 sudo apt install -y libcanberra-gtk-module mesa-utils mesa-utils-extra
 
 # sudo apt install libeigen3-dev
