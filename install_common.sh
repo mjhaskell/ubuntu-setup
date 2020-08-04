23ccf8115aa86d2c4b59d205ca9223cda51ee3cb
Mat Haskell <mhaskell9@gmail.com>
2020-08-04 14:03:40 -0600

    Updated packages

--- install_common.sh
+++ install_common.sh
@@ -5,14 +5,15 @@ sudo apt upgrade
 
 echo_blue "installing common"
 
-sudo apt install -y git zsh curl htop ssh gcc cmake ccache make gdb
-sudo apt install -y tmux xclip qpdfview shotwell unzip
+sudo apt install -y zsh curl htop ssh gcc cmake ccache make gdb
 sudo apt install -y build-essential manpages-dev software-properties-common
-sudo apt install -y exfat-utils exfat-fuse
-sudo apt install -y python-dev python-pip python3-dev python3-pip python3-tk
-sudo apt install -y pybind11-dev libtool  
-sudo apt install -y libffi6 libffi-dev 
+sudo apt install -y xserver-xorg-input-libinput x11-xserver-utils xinit
+sudo apt install -y xterm rxvt-unicode
+sudo apt install -y tmux xclip unzip
+sudo apt install -y python-dev python-pip python3-dev python3-pip # python3-tk
+sudo apt install -y pybind11-dev libtool libffi-dev
 sudo apt install -y nmap cifs-utils
+sudo apt install -y qpdfview shotwell
 sudo apt install -y gnome-screenshot simplescreenrecorder vlc ffmpeg
 sudo apt install -y libcanberra-gtk-module mesa-utils mesa-utils-extra
 
