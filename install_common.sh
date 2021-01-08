52201b6f392e2c06eb70613fa022888e565bfa67
Mat Haskell <mhaskell9@gmail.com>
2021-01-08 11:46:00 -0700

    Reorganized and updated for Ubuntu 20.04

--- install_common.sh
+++ install_common.sh
@@ -12,29 +12,65 @@ sudo passwd root
 
 echo_blue "installing common"
 
-sudo apt install -y zsh curl htop ssh gcc cmake ccache make gdb
-sudo apt install -y build-essential manpages-dev software-properties-common
+# network (use 'sudo nmtui' to configure wifi)
+sudo systemctl disable systemd-networkd.service
+sudo apt install -y network-manager nmap cifs-utils
+
+# terminal
+sudo apt install -y git zsh curl htop ssh 
+sudo apt install -y manpages-dev software-properties-common
 sudo apt install -y xterm rxvt-unicode
 sudo apt install -y tmux xclip unzip
-sudo apt install -y python-dev python-pip python3-dev python3-pip # python3-tk
+sudo apt install -y fonts-emojione
+
+# graphics
+sudo apt install -y mesa-utils mesa-utils-extra
+
+# sounds
+sudo apt install -y libcanberra-gtk-module
+sudo apt install -y alsa-base alsa-utils
+
+# windows file system (fs)
+sudo apt install -y ntfs-3g
+
+# C++
+sudo apt install -y build-essential gcc g++ gcc-10 g++-10 cmake ccache make gdb
+# set up compiler versions (last # is priority - highest is default)
+# change compilers with `sudo update-alternatives --config <g++>`
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
+echo_yellow "G++ compiler information:"
+sudo update-alternatives --display g++
+
+# python deps
+sudo apt install -y python3-dev python3-venv
 sudo apt install -y pybind11-dev libtool libffi-dev
-sudo apt install -y nmap cifs-utils
-sudo apt install -y qpdfview shotwell
-sudo apt install -y gnome-screenshot simplescreenrecorder vlc ffmpeg
-sudo apt install -y libcanberra-gtk-module mesa-utils mesa-utils-extra
+# pygame deps
+sudo apt install -y libfreetype-dev libsdl-image1.2-dev libsdl-mixer1.2-dev
+sudo apt install -y libsdl1.2-dev libsdl-ttf2.0-dev libportmidi-dev
 
-# sudo apt install libeigen3-dev
+# pdf, image, and video viewers
+sudo apt install -y qpdfview shotwell vlc ffmpeg
 
-if [ ! -d ~/software ]; then mkdir ~/software; fi
-if [ ! -d ~/bin ]; then mkdir ~/bin; fi
+# screenshot and screen recording
+sudo apt install -y gnome-screenshot simplescreenrecorder 
 
 echo_green "common installed"
 
+# create my common folder structure
+if [ ! -d ~/software ]; then mkdir ~/software; fi
+if [ ! -d ~/bin ]; then mkdir ~/bin; fi
+
+# add user to groups
 sudo usermod -a -G dialout $USER
 sudo usermod -a -G plugdev $USER
+sudo usermod -a -G video $USER
 
 echo_green "user added to common groups"
 
+# set the timezone
 sudo timedatectl set-timezone America/Denver
 
 echo_green "timezone set to MDT"
