c6da5c51e46f421e453842f481b17083821b5142
Mat Haskell <mhaskell9@gmail.com>
2021-02-12 15:59:49 -0700

    Added some zsh plugins

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -4,4 +4,7 @@ echo_blue "configuring zsh with oh-my-zsh"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
 
