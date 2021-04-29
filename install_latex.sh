dc8a2cf2a787a737113113729bc805317fc456e4
Mat Haskell <mhaskell9@gmail.com>
2021-04-28 21:42:43 -0600

    Only install pieces I use

--- install_latex.sh
+++ install_latex.sh
@@ -3,7 +3,9 @@
 echo_blue "intalling latex"
 
 #sudo apt install texlive-full texmaker
-sudo apt install -y texlive-latex-extra
+sudo apt install -y texlive-latex-extra texlive-font-utils biber
+sudo apt install -y texlive-bibtex-extra lmodern latexmk
+sudo apt install -y texlive-luatex texlive-xetex
 
 echo_green "latex installed"
 
