0321fc7296fdc1cf7f78a640d901cdc3529aca1e
Mat Haskell <mhaskell9@gmail.com>
2021-05-01 23:03:16 -0600

    Do not install recommends to prevent issues with full gdm

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -2,9 +2,9 @@
 
 echo_blue "installing i3"
 
-sudo apt install -y xserver-xorg-input-libinput x11-xserver-utils
+sudo apt install --no-install-recommends -y xserver-xorg-input-libinput x11-xserver-utils
 sudo add-apt-repository ppa:kgilmer/speed-ricer
-sudo apt install -y i3-gaps-wm i3status rofi
-sudo apt install -y xinit
+sudo apt install --no-install-recommends -y i3-gaps-wm i3status rofi
+sudo apt install --no-install-recommends -y xinit
 
 echo_green "i3 installed"
