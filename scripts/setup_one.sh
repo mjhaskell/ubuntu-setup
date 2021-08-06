ea8f30ec817df528075e8e424ad6fffe48300024
Mat Haskell <mhaskell9@gmail.com>
2021-08-06 00:01:59 -0600

    Updated setup scripts

--- scripts/setup_one.sh
+++ scripts/setup_one.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
-SETUP_DIR="$( cd $SCRIPT_DIR/.. && pwd )"
 
 sh $SCRIPT_DIR/install_common.sh
 sh $SCRIPT_DIR/setup_sshkey.sh
@@ -9,9 +8,3 @@ sh $SCRIPT_DIR/setup_keybindings.sh
 sh $SCRIPT_DIR/install_dotfiles.sh
 sh $SCRIPT_DIR/install_vim.sh
 sh $SCRIPT_DIR/upgrade_pips.sh
-
-#bash install_latex.sh
-#bash install_eigen.sh
-#bash install_OSG.sh
-#bash install_gimp.sh
-#bash install_lyx.sh
--- scripts/setup_two.sh
+++ scripts/setup_two.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
-SETUP_DIR="$( cd $SCRIPT_DIR/.. && pwd )"
 
-INSTALL_ARR="install_pips.sh"
+INSTALL_ARR=""
+
 
 ask_to_install()
 {
@@ -23,29 +23,42 @@ ask_to_install()
   # return $INSTALL
 }
 
-ask_to_install "LaTeX" install_latex.sh
-INSTALL_LATEX=$?
+
+ask_to_install "Cuda" install_cuda.sh
+ask_to_install "NvidiaDriver" install_nvidia_driver.sh
+
+ask_to_install "i3" install_i3.sh
+ask_to_install "Termite" install_termite.sh
 
 ask_to_install "Eigen" install_eigen.sh
-INSTALL_EIGEN=$?
-
-ask_to_install "OpenSceneGraph" install_OSG.sh
-INSTALL_OSG=$?
-
-ask_to_install "Gimp" install_gimp.sh
-INSTALL_GIMP=$?
-
-ask_to_install "Lyx" install_lyx.sh
-INSTALL_LYX=$?
-
 ask_to_install "GTest" install_gtest.sh
-INSTALL_GTEST=$?
+ask_to_install "OSQP" install_osqp.sh
+ask_to_install "LaTeX" install_latex.sh
+ask_to_install "Lyx" install_lyx.sh
+ask_to_install "VSCode" install_vscode.sh
 
+ask_to_install "Signal" install_signal.sh
+ask_to_install "Zoom" install_zoom.sh
+
+ask_to_install "QT" install_qt.sh
+ask_to_install "OpenSceneGraph" install_OSG.sh
+ask_to_install "Gimp" install_gimp.sh
 ask_to_install "OpenCV" install_opencv.sh
-INSTALL_OPENCV=$?
-
 ask_to_install "ROS" install_ros_noetic.sh
-INSTALL_ROS=$?
+ask_to_install "Holodeck" install_holodeck.sh
 
-echo_red "computer will now power off"
-reboot
+ask_to_install "zsh" setup_zsh.sh
+
+
+# sh $SCRIPT_DIR/install_common.sh
+# sh $SCRIPT_DIR/setup_sshkey.sh
+# sh $SCRIPT_DIR/setup_keybindings.sh
+# sh $SCRIPT_DIR/install_dotfiles.sh
+# sh $SCRIPT_DIR/install_vim.sh
+# sh $SCRIPT_DIR/install_pips.sh
+
+for script in $INSTALL_ARR; do
+  sh $SCRIPT_DIR/$script
+done
+
+echo_purple "You should probably reboot now"
