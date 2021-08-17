b2d57c18e0ec7ced67b4dd6f3b8d862b67bf754a
Mat Haskell <mhaskell9@gmail.com>
2021-08-17 14:38:35 -0600

    Add -y flag to PPA packages

--- scripts/install_OSG.sh
+++ scripts/install_OSG.sh
@@ -2,7 +2,7 @@
 
 echo_blue "installing openscenegraph"
 
-sudo add-apt-repository ppa:openmw/openmw
+sudo add-apt-repository -y ppa:openmw/openmw
 sudo apt update
 sudo apt install -y openscenegraph-3.4 libopenscenegraph-3.4-dev
 
--- scripts/install_gimp.sh
+++ scripts/install_gimp.sh
@@ -2,7 +2,7 @@
 
 echo_blue "installing gimp"
 
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 sudo apt update
 sudo apt install -y gimp
 
