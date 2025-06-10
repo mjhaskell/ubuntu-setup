178eb13e930d444ff971f477ed5974ca8424d3a2
Mat Haskell <mhaskell9@gmail.com>
2025-06-09 21:32:25 -0600

    Customized dashboard

--- dotfiles/nvim/lua/plugins/alpha.lua
+++ dotfiles/nvim/lua/plugins/alpha.lua
@@ -16,16 +16,26 @@ return {
       "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
       "                                                     ",
     }
- 
+
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("n", "  > New file", "<cmd>ene<CR>"),
       --dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+      -- dashboard.button("s", "  > Open session", "<cmd>SessionSearch<CR>"),
+      dashboard.button("r", "󰁯  > Restore session", "<cmd>SessionSearch<CR>"),
+      dashboard.button("e", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
+      dashboard.button("f", "󰱼  > Find file", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("g", "󰍉  > Grep", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("c", "  > Find config file", function()
+        local builtin = require("telescope.builtin")
+        builtin.find_files({
+          -- search_dir = { os.getenv("HOME") .. "/.config/nvim" },
+          cwd = os.getenv("HOME") .. "/.config/nvim",
+        })
+      end),
+      -- dashboard.button("r", "󰁯  > Restore session for cwd", "<cmd>SessionRestore<CR>"),
+      dashboard.button("L", "󰒲  > Open Lazy", "<cmd>Lazy<CR>"),
+      dashboard.button("Q", "  > Quit NVIM", "<cmd>qa<CR>"),
     }
 
     -- Send config to alpha
