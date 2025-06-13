7906718c121fc2c33925ff1346d022fc22053aaf
mat <mat@xbud.app.byu.edu>
2025-06-13 15:41:27 -0600

    Add blink to nvim config
    Blink required a newer version of rust that I now have. Blink should
    give autocompletion and open docstrings when inside of function ().

--- dotfiles/nvim/lua/plugins/lsp/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lsp/lspconfig.lua
@@ -31,24 +31,24 @@ return {
       end,
     },
 
-    -- { -- optional blink completion source for require statements and module annotations
-    --   "saghen/blink.cmp",
-    --   build = "cargo build --release",
-    --   opts = {
-    --     sources = {
-    --       -- add lazydev to your completion providers
-    --       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-    --       providers = {
-    --         lazydev = {
-    --           name = "LazyDev",
-    --           module = "lazydev.integrations.blink",
-    --           -- make lazydev completions top priority (see `:h blink.cmp`)
-    --           score_offset = 100,
-    --         },
-    --       },
-    --     },
-    --   },
-    -- },
+    { -- optional blink completion source for require statements and module annotations
+      "saghen/blink.cmp",
+      build = "cargo build --release",
+      opts = {
+        sources = {
+          -- add lazydev to your completion providers
+          default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+          providers = {
+            lazydev = {
+              name = "LazyDev",
+              module = "lazydev.integrations.blink",
+              -- make lazydev completions top priority (see `:h blink.cmp`)
+              score_offset = 100,
+            },
+          },
+        },
+      },
+    },
   },
 
   config = function()
