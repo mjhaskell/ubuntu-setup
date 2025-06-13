2aaaefc55948cdf49996396289eaf7f4b536cf5e
mat <mat@xbud.app.byu.edu>
2025-06-13 15:49:56 -0600

    Update default installed packages

--- scripts/install_common.sh
+++ scripts/install_common.sh
@@ -16,6 +16,7 @@ sudo apt upgrade -y
 if [ ! -d ~/software ]; then mkdir ~/software; fi
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 if [ ! -d ~/tmp ]; then mkdir ~/tmp; fi
+if [ ! -d ~/scratch ]; then mkdir ~/scratch; fi
 
 # install color functions
 sudo cp $SETUP_DIR/bin/color_functions/echo_* /usr/local/bin/
@@ -26,53 +27,60 @@ echo_blue "Creating password for root"
 sudo passwd root
 
 # network (use 'sudo nmtui' to configure wifi)
-sudo systemctl disable systemd-networkd.service
-sudo apt install -y network-manager nmap
+# sudo systemctl disable systemd-networkd.service
+# sudo apt install -y network-manager nmap
+sudo apt install -y nmap
 
 # terminal
-sudo apt install -y git zsh curl htop ssh
-sudo apt install -y manpages-dev software-properties-common
+sudo apt install -y git zsh curl htop ssh # htop already installed
+sudo apt install -y bat ripgrep fd-find
+# sudo apt install -y manpages-dev software-properties-common # already installed
 # sudo apt install -y xterm rxvt-unicode
-# sudo apt install -y tmux xclip unzip
-sudo apt install -y xclip unzip
+sudo apt install -y tmux xclip unzip # unzip already installed
+# sudo apt install -y xclip unzip
 sudo apt install -y fonts-emojione
 
 # graphics
-sudo apt install -y mesa-utils mesa-utils-extra
+# mesa-utils-extra no longer exists
+# mesa-utils already installed
+sudo apt install -y mesa-utils # mesa-utils-extra
 
 # sounds
 sudo apt install -y libcanberra-gtk-module
-sudo apt install -y alsa-base alsa-utils
+sudo apt install -y alsa-base alsa-utils # already installed
 
 # printers
-sudo apt install -y hplip
+sudo apt install -y hplip # already installed
 
 # windows file system (fs)
-sudo apt install -y ntfs-3g
+sudo apt install -y ntfs-3g # already installed
 # tools for mounting J-drive
 sudo apt install -y cifs-utils
 
 # C++
-sudo apt install -y build-essential gcc g++ gcc-12 g++-12 gdb
-sudo apt install -y clang-15 lldb-15
+sudo apt install -y build-essential gcc g++ gcc-14 g++-14 gdb # already installed
+sudo apt install -y clang-19 lldb-19 # clang (default) gives v18
 sudo apt install -y cmake ccache make cmake-curses-gui
 # set up compiler versions (last # is priority - highest is default)
 # change compilers with `sudo update-alternatives --config <g++>`
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 11
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 12
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-12 12
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 13
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 13
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 14
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 14
 echo_yellow "G++ compiler information:"
 sudo update-alternatives --display g++
 
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-15 15
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-15 15
-sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-15 15
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 18
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-18 18
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-18 18
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-19 19
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-19 19
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-19 19
 echo_yellow "G++ compiler information:"
 sudo update-alternatives --display clang++
 
 # python deps
-sudo apt install -y python3-dev python3-venv python3-tk
+sudo apt install -y python3-dev python3-venv # python3-tk
 sudo apt install -y pybind11-dev libtool libffi-dev
 # create default virtual environment
 if [ ! -d ~/.pyvenvs ]; then
@@ -81,15 +89,21 @@ if [ ! -d ~/.pyvenvs ]; then
   python3 -m venv ${HOME}/.pyvenvs/default
 fi
 
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+ 
+# Node.js
+sudo apt install npm
+
 # pdf, image, and video viewers
 # sudo apt install -y qpdfview
-sudo apt install -y zathura
-sudo apt install -y shotwell
+# sudo apt install -y zathura
+# sudo apt install -y shotwell
 sudo apt install -y vlc ffmpeg
 
 # screenshot and screen recording
 sudo apt install -y flameshot
-sudo apt install -y simplescreenrecorder
+# sudo apt install -y simplescreenrecorder
 
 echo_green "common installed"
 
