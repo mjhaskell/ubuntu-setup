07610de5b4b4647ddbf2f52e36abbec165b6ce47
Mat Haskell <mhaskell9@gmail.com>
2025-12-16 16:37:41 -0700

    Updated zsh config

--- dotfiles/zshconf/plugins_ubuntu.zsh
+++ dotfiles/zshconf/plugins_ubuntu.zsh
@@ -1,12 +1,17 @@
+## Ubuntu
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
 fpath=($fpath $HOME/software/zsh-completions/src)
+
 # autoload -Uz compinit # not sure if this is needed
 # compinit
 # rm -f ~/.zcompdump; compinit
 
-# ---
+## Arch
+# source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+## All
 # zsh_syntax_highlighter customization
 # ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern regexp)
 
@@ -20,7 +25,7 @@ ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
 ## ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
 ## ZSH_HIGHLIGHT_STYLES[path]='fg=#0093ad'
 ## ZSH_HIGHLIGHT_STYLES[path]='fg=31'
-#ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
 #
 ## # To disable highlighting of globbing expressions
 ## ZSH_HIGHLIGHT_STYLES[globbing]='none'
--- dotfiles/zshconf/prompt.zsh
+++ dotfiles/zshconf/prompt.zsh
@@ -3,9 +3,10 @@ promptinit
 
 setopt prompt_subst
 
+
 ###### SYNTAX
-# see https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
-# for more options
+## see https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
+## for more options
 #
 # %F{color}     - start using foreground <color>
 # %f            - end changing foreground color
@@ -18,18 +19,71 @@ setopt prompt_subst
 # %u            - end underline mode
 #
 # %~            - CWD (current working directory)
+# %#            - prompt symbol (defaults = root: #, user: %)
 # %n            - USERNAME
 # %m            - HOSTNAME up to first '.' (can add an int after % to specify how many characters and it can be negative
 # to go from the end)
 # %M            - full HOSTNAME
 #
-# custom colors
+## custom colors
 # typeset -H my_grey="%F{237}"
+#
+## ternary expressions
+# %(!.true.false)  # test if root user
+# %(?.true.false)  # test if error from prev command
 ######
 
 ## could also try to use exported info from git (vcs_info)
 # https://git-scm.com/book/en/v2/Appendix-A%3A-Git-in-Other-Environments-Git-in-Zsh
 
+
+################# Setting Window Titles ########################################
+# https://unix.stackexchange.com/a/10533
+# https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#SEC52
+# section 13.3 --> conditional substrings in prompts
+# https://zsh.sourceforge.io/Doc/Release/Functions.html
+# section 9.3 --> preexec
+display_path() {
+  if [ "$SSH_TTY" ]; then # change prompt when in ssh
+    print -Pn "@%m:%~ %(!.#.)"
+  else
+    print -Pn "%~ %(!.#.»)"
+  fi
+}
+set_window_title() {
+  if [ $# -eq 2 ]; then  # $1 should be term cmd
+    printf "%b%s %s%b" "\e]0;" "$1" "$2" "\007"
+  else
+    printf "%b%s%b" "\e]0;" "$1" "\a"
+  fi
+}
+
+case $TERM in
+  termite|foot*|*xterm*|rxvt|rxvt-unicode|rxvt-256color|rxvt-unicode-256color|(dt|k|E)term)
+    function precmd () {
+     set_window_title "$(display_path)"
+    }
+    function preexec () {
+      local cmd=$1
+      set_window_title "$(display_path)" "$cmd"
+    }
+    ;;
+  # screen|screen-256color)
+  #   function precmd () {
+  #     print -Pn "\e]83;title \"$1\"\a"
+  #     print -Pn "\e]0;$TERM - (%L) %# @%m:%~\a"
+  #     git_prompt_info
+  #   }
+  #   function preexec () {
+  #     print -Pn "\e]83;title \"$1\"\a"
+  #     print -Pn "\e]0;$TERM - (%L) %# @%m:%~ ($1)\a"
+  #   }
+  #   ;;
+esac
+################# Setting Window Titles ########################################
+
+
+################# Setting Zsh Prompt ###########################################
 git_prompt_info() {
   local GIT_PROMPT_PREFIX="%F{yellow}"
   local GIT_PROMPT_SUFFIX="%f"
@@ -48,73 +102,42 @@ git_prompt_info() {
   fi
 }
 
-# case $TERM in
-#   termite|foot*|*xterm*|rxvt|rxvt-unicode|rxvt-256color|rxvt-unicode-256color|(dt|k|E)term)
-#     function precmd () {
-#       print -Pn "\e]0; %# @%m:%~\a"
-#     }
-#     function preexec () { print -Pn "\e]0; %# @%m:%~ %(!.#.») $1\a" }
-#     ;;
-#   screen|screen-256color)
-#     function precmd () {
-#       print -Pn "\e]83;title \"$1\"\a"
-#       print -Pn "\e]0;$TERM - (%L) %# @%m:%~\a"
-#       git_prompt_info
-#     }
-#     function preexec () {
-#       print -Pn "\e]83;title \"$1\"\a"
-#       print -Pn "\e]0;$TERM - (%L) %# @%m:%~ ($1)\a"
-#     }
-#     ;;
-# esac
-
+prompt_venv() {
+  [[ -n "$VIRTUAL_ENV" ]] && print -n "(${VIRTUAL_ENV:t}) " || print -n ""
+}
 
 prompt_custom_theme_setup() {
   # local return_code="%B%(?.%F{green}%f.%F{red}%? ↵%b%f)"
   local return_code="%B%(?..%F{red}%? ↵%b%f)"
 
-  if [[ $UID -eq 0 ]]; then # for root
-      local user="%B%F{red}%n%b%f"
-      local symbol="%B%F{red}#%b%f"
-  else
-      local user="%B%F{green}%n%b%f"
-      local symbol="%B%F{cyan}>%b%f"
-  fi
-
+  local user symbol host
   if [ "$SSH_TTY" ]; then # change prompt when in ssh
-      # 󱥠 󰿆 󱦚 󱥡 󰉑 󱪧 󱦛
-      #local host="@%F{magenta}%m%F{yellow}󱥠%f:" #(ssh)
-      local host="%F{magenta}%m%f" #(ssh)
-      local symbol="%B%F{cyan} %b%f"
-      local user="%B%F{green}DBA %n%b%f"
+    # 󱥠 󰿆 󱦚 󱥡 󰉑 󱪧 󱦛
+    user="%B%F{green}DBA %n%b%f"
+    symbol="%B%F{cyan} %b%f"
+    host="%F{magenta}%m%f" #(ssh)
+    #host="@%F{magenta}%m%F{yellow}󱥠%f:" #(ssh)
   else
-      local host=''
+    user="%(!.%B%F{red}%n%b%f.%B%F{green}%n%b%f)"
+    symbol="%(!.%B%F{red}%#%b%f.%B%F{cyan}>%b%f)"
+    host=""
   fi
+  
+  venv="$(prompt_venv)"
+  cwd="%B%F{cyan}%~%b%f"
+  git_info="$(git_prompt_info)"
 
-  if [[ "${VIRTUAL_ENV_PROMPT[1]}" == "(" ]]; then
-    local venv="${VIRTUAL_ENV_PROMPT}"
-  else
-    local venv="(${VIRTUAL_ENV_PROMPT}) "
-  fi
-  # local venv="${VIRTUAL_ENV_PROMPT}"
-  local cwd="%B%F{cyan}%~%b%f"
-  local git_info="$(git_prompt_info)"
-
-  local line1="${venv}${user}: ${cwd} ${git_info}"
-  local line2="${host}${symbol} " 
-  local newline=$'\n'
+  line1="${venv}${user}: ${cwd} ${git_info}"
+  line2="${host}${symbol} " 
+  newline=$'\n'
 
   PROMPT="${line1}${newline}${line2}"
   RPS1="${return_code}"
 }
-
-## can use precmd, but might be better to append to list of precmds
-#precmd() {
-#  prompt_custom_theme_setup
-#}
 
 precmd_functions+=(prompt_custom_theme_setup)
 
 ## can also use zsh hook to add to list of precmds
 # autoload -Uz add-zsh-hook
 # add-zsh-hook precmd prompt_custom_theme_setup
+################# Setting Zsh Prompt ###########################################
