8407efc419c306c5f8f7beceb0477ff9b11b46e1
Mat Haskell <mhaskell9@gmail.com>
2025-12-18 21:14:53 -0700

    Add libva to nvidia installation
    The libva packages seemed to help me be able to set a dark system theme

--- install/arch/nvidia.zsh
+++ install/arch/nvidia.zsh
@@ -1,19 +1,28 @@
 #!/usr/bin/zsh
 
+# see:
+#   https://wiki.archlinux.org/title/NVIDIA
+#   https://wiki.archlinux.org/title/Hardware_video_acceleration
+#   https://wiki.archlinux.org/title/Vulkan
+
 SCRIPT_DIR="$(cd "$(dirname $0)" && pwd)"
 SETUP_DIR="$(cd $SCRIPT_DIR/.. && pwd)"
 
 echo_blue "Installing nvidia"
 
 ### see Arch wiki on nvidia for options
-# sudo pacman -S nvidia
+# sudo pacman -S nvidia  # TODO: decide on open vs proprietary
 sudo pacman -S --needed nvidia-open
+# TODO: check if using linux-lts kernel and install nvidia-open-lts if so
 
 sudo pacman -S --needed nvidia-utils
 sudo pacman -S --needed mesa-utils
 
-sudo pacman -S --needed vulkan-tools
-sudo pacman -S --needed vulkan-extra-layers vulkan-extra-tools vulkan-utility-libraries
+# hardware video acceleration
+sudo pacman -S --needed libva-nvidia-driver libva-utils
+
+sudo pacman -S --needed vulkan-tools vulkan-extra-tools 
+sudo pacman -S --needed vulkan-extra-layers vulkan-utility-libraries
 sudo pacman -S --needed vulkan-headers vulkan-gfxstream
 sudo pacman -S --needed vulkan-validation-layers vulkan-virtio
 
